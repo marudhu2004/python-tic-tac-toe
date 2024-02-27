78218fe88d8f7bad31bb239e822baff129c63db5
Marudhu Paandian.K <46159834+marudhu2004@users.noreply.github.com>
2024-02-27 15:12:42 +0530

    added the template for board and ai

---
+++ tic_tac_toe/board.py
@@ -0,0 +1,25 @@
+class Board:
+
+    def __init__(self, player: bool) -> None:
+        pass
+
+    def make_move(self, move):
+        pass
+
+    def list_free_moves(self):
+        pass
+
+    def has_winner(self):
+        pass
+
+    def game_over(self):
+        pass
+
+    def get_player(self):
+        pass
+
+    def get_board(self):
+        pass
+
+    def make_copy(self):
+        pass
