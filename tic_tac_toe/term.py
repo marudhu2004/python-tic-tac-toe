1918d2f7400e515aca6ef1d67fa2d1c0472fe7e1
Sriram Tantri <80676365+sriram20050@users.noreply.github.com>
2024-02-27 14:20:05 +0530

    Added Banner to the code

--- tic_tac_toe/term.py
+++ tic_tac_toe/term.py
@@ -5,3 +5,10 @@ def help():
           you take turns with the other player trying to outwit each other
           .If the board is full without a winner, consider it a draw""")
     
+def banner():
+    print("Welcome to Tic Tac Toe.")
+    print("[x][o][x]")
+    print("[o][x][x]")
+    print("[o][o][o]")
+    print("Developed by Marudhu2004 and Sidz2007.")
+     
