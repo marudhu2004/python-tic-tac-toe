3f73b533c263696057bdd6e9041d35f4a9b44f44
Marudhu Paandian.K <46159834+marudhu2004@users.noreply.github.com>
2024-02-27 14:04:19 +0530

    implemented basic help message

---
+++ tic_tac_toe/term.py
@@ -0,0 +1,7 @@
+def help():
+    print("If you don't know how to play the game, google it!")
+    print("""there is a 3x3 grid where you face your opponent
+          and try and get 3 x or o on the same row, column or diagonal
+          you take turns with the other player trying to outwit each other
+          .If the board is full without a winner, consider it a draw""")
+    
